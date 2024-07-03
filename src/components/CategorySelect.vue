0a2da72e95586e4e5adba86f15e5ca0f185595d7
Khmelev7koiOleh <152546309+Khmelev7koiOleh@users.noreply.github.com>
2024-07-03 07:15:17 +0200

    Add files via upload

---
+++ src/components/CategorySelect.vue
@@ -0,0 +1,32 @@
+<script setup>
+import { ref, toRefs } from "vue";
+import { RouterLink } from "vue-router";
+import uniqolor from "uniqolor";
+
+let randColor = ref("");
+randColor.value = uniqolor.random("");
+
+const props = defineProps({
+  category: String,
+  image: String,
+});
+const { category } = toRefs(props);
+</script>
+<template>
+  <RouterLink>
+    <div
+      v-if="randColor"
+      :style="`background-color:${randColor.color};`"
+      class="conrtast-[2] brightness-[0.87] aspect-square rounded-xl relative overflow-hidden hover:opacity-[70%]"
+    >
+      <div class="text-white font-semibold pl-4 pt-3 text-[23px]">
+        {{ category }}
+      </div>
+      <img
+        class="absolute contrast-[0.55] -right-3.5 bottom-0 max-w-[100px] rotate-[20deg]"
+        :src="image || ''"
+        alt=""
+      />
+    </div>
+  </RouterLink>
+</template>
---
+++ src/components/HomeCard.vue
@@ -0,0 +1,26 @@
+<script setup>
+import { toRefs } from "vue";
+import { RouterLink } from "vue-router";
+
+const props = defineProps({
+  image: String,
+  title: String,
+  subTitle: String,
+});
+
+const { image, title, subTitle } = toRefs(props);
+</script>
+
+<template>
+  <RouterLink to="library">
+    <div
+      class="bg-[#121212] rounded-md hover:bg-[#252525] p-4 m-2 cursor-pointer"
+    >
+      <img class="rounded-md" :src="image" alt="" />
+      <div class="text-white font-semibold pt-4 text-[17px]">{{ title }}</div>
+      <div class="text-gray-400 pt-1 pb-4 text-[14px]">
+        {{ subTitle }}
+      </div>
+    </div>
+  </RouterLink>
+</template>
